8bac142b7774f858473d2fd2bf6e24fb6da5a684
Tjcarroll1 <tjcarroll1@icloud.com>
2024-10-27 17:37:05 +1000

    added menu schema

---
+++ api/schemaTypes/menuPage.ts
@@ -0,0 +1,66 @@
+import {defineType, defineField} from 'sanity'
+
+export const menuPage = defineType({
+  name: 'menuPage',
+  title: 'Menu Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'menuItems',
+      title: 'Menu Items',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'menuItem'}]
+        }
+      ]
+    }),
+  ],
+})
+
+export const menuItem = defineType({
+  name: 'menuItem',
+  title: 'Menu Item',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
