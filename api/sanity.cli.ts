c21de41d11f32bfd93b2be03ece807e66d76f087
Tjcarroll1 <tjcarroll1@icloud.com>
2024-10-27 16:59:52 +1000

    first front end commit

--- api/sanity.cli.ts
+++ api/sanity.cli.ts
@@ -1,13 +1,16 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'jyn0aimf',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
-})
+
+  // Add the studioHost to avoid hostname prompts during deployment
+  studioHost: 'client-demo',
+});
